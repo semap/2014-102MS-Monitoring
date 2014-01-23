3daf0699f4df837e9afb64123619ab31129efb40
Shaun Luttin <shaunluttin@gmail.com>
2014-01-22 18:35:40 -0800

    Query some of the main hardware of the Cobra.

--- MFConsoleApplication1/MFConsoleApplication1/Program.cs
+++ MFConsoleApplication1/MFConsoleApplication1/Program.cs
@@ -1,7 +1,6 @@
-﻿using Microsoft.SPOT;
-using Microsoft.SPOT.Hardware;
-using GHI.Premium.Net;
-using GHI.Hardware.G120;
+﻿using SpotBase = Microsoft.SPOT;
+using SpotIO = Microsoft.SPOT.IO;
+using SpotWare = Microsoft.SPOT.Hardware;
 
 namespace MFConsoleApplication1
 {
@@ -9,8 +8,47 @@ namespace MFConsoleApplication1
     {
         public static void Main()
         {
-            Debug.Print(
-                Resources.GetString(Resources.StringResources.String1));
+            SpotBase.Debug.Print(
+                Resources.GetString(Resources.StringResources.HelloWorld));
+
+            SpotBase.Debug.Print("Volumes:" + SpotIO.VolumeInfo.GetVolumes().Length.ToString());
+            SpotBase.Debug.Print("SystemInfo.Version:" + SpotWare.SystemInfo.Version);
+            SpotBase.Debug.Print("SystemInfo.OEMString:" + SpotWare.SystemInfo.OEMString);
+
+            const string todo = "todo";
+            SpotWare.HardwareProvider hwp = SpotWare.HardwareProvider.HwProvider;
+            SpotBase.Debug.Print("GetAnalogChannelsCount:" + hwp.GetAnalogChannelsCount());
+            SpotBase.Debug.Print("GetAnalogOutputChannelsCount:" + hwp.GetAnalogOutputChannelsCount());
+            SpotBase.Debug.Print("GetAnalogOutputPinForChannel:" + todo);
+            SpotBase.Debug.Print("GetAnalogPinForChannel:" + todo);
+            SpotBase.Debug.Print("GetAvailableAnalogOutputPrecisionInBitsForChannel:" + todo);
+            SpotBase.Debug.Print("GetAvailablePrecisionInBitsForChannel:" + todo);
+            SpotBase.Debug.Print("GetButtonPins:" + hwp.GetButtonPins(SpotWare.Button.LastSystemDefinedButton));
+            SpotBase.Debug.Print("GetPWMChannelsCount:" + hwp.GetPWMChannelsCount());
+            SpotBase.Debug.Print("GetPinsCount:" + hwp.GetPinsCount());
+            SpotBase.Debug.Print("GetPinsUsage:" + hwp.GetPinsUsage(SpotWare.Cpu.Pin.GPIO_Pin0));
+            SpotBase.Debug.Print("GetPwmPinForChannel:" + todo);
+            SpotBase.Debug.Print("GetSerialPortsCount:" + hwp.GetSerialPortsCount());
+            SpotBase.Debug.Print("GetSpiPortsCount:" + hwp.GetSpiPortsCount());
+            SpotBase.Debug.Print("GetSupportedInterruptModes:" + todo);
+            SpotBase.Debug.Print("GetSupportedResistorModes:" + todo);
+            SpotBase.Debug.Print("IsSupportedBaudRate:" + todo);
+            SpotBase.Debug.Print("GetI2CPins:" + todo);
+            SpotBase.Debug.Print("GetLCDMetrics:" + todo);
+
+            SpotWare.Cpu.PinUsage[] pinUsuage;
+            int pinCount;
+            hwp.GetPinsMap(out pinUsuage, out pinCount);
+            SpotBase.Debug.Print("GetPinsMap:" + pinCount);
+            SpotBase.Debug.Print("GetSerialPins:" + todo);
+            SpotBase.Debug.Print("GetSpiPins:" + todo);
+            SpotBase.Debug.Print("GetSupportBaudRates:" + todo);
+        }
+
+        // https://www.ghielectronics.com/docs/48/fez-cobra-ii-developer#425
+        private static void WiFiExample()
+        {
+        
         }
     }
 }
