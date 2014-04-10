2a2936241d4e1fc93dfc85b02dba53f33a81c2b1
Shaun Luttin <shaunluttin@gmail.com>
2014-04-09 17:53:16 -0700

    Comment code.

--- Algae.WcfCobraTestClient02/Program.cs
+++ Algae.WcfCobraTestClient02/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace Algae.WcfCobraTestClient02
+﻿////#define UseTestClass
+
+namespace Algae.WcfCobraTestClient02
 {
     using System;
 
@@ -8,16 +10,20 @@
         {
             WatchdogWrapper.Watch();
 
-            // start program
+            // put any code that we want to watch in the try block
+            // this mimics global error handling
             try
             {
-                ////WatchdogTestClass p = new WatchdogTestClass();
-
-                AlgaeMonitor m = new AlgaeMonitor(new Network());
-                m.SendContinuousTestDataAcrossNetwork();
+#if UseTestClass
+                    WatchdogTestClass p = new WatchdogTestClass();
+#else
+                    AlgaeMonitor m = new AlgaeMonitor(new Network());
+                    m.SendContinuousTestDataAcrossNetwork();
+#endif
             }
             catch (Exception ex)
             {
+                // reboot on unhandle exceptions
                 SdCard.WriteException(ex);
                 WatchdogWrapper.ForceReboot();
             }
