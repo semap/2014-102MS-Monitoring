d5fcd3fffe40399ed9d5ef029d4458603994b756
Shaun Luttin <admin@shaunluttin.com>
2016-01-29 16:12:17 -0800

    Organize into 4.3 and 4.2 folders.

---
+++ NETMF4.3/Algae/Algae.Main/Program.cs
@@ -0,0 +1,17 @@
+using System;
+using Microsoft.SPOT;
+
+namespace Algae.Main
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Debug.Print("Program started.");
+
+
+
+            Debug.Print("Program finished.");
+        }
+    }
+}
