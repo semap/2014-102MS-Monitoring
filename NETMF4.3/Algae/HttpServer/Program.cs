13cd2994aadd111388a4a7d3cb53164d01cbb813
Shaun Luttin <admin@shaunluttin.com>
2016-02-05 12:12:41 -0800

    Serve HTTP from emulator.

---
+++ NETMF4.3/Algae/HttpServer/Program.cs
@@ -0,0 +1,13 @@
+using System;
+using Microsoft.SPOT;
+
+namespace HttpServer
+{
+    public static class Program
+    {
+        public static void Main()
+        {
+            new HttpServer(true);
+        }
+    }
+}
